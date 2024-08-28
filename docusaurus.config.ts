c5507df40fa8004ce1b626917834d09f08d1d73a
Ghost_chu <ghostchu@111.com>
2024-08-29 01:22:10 +0800

    提交更改

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,12 +11,12 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/pbh-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'PBH-BTN', // Usually your GitHub org/user name.
-  projectName: 'PeerBanHelper', // Usually your repo name.
+  projectName: 'pbh-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
