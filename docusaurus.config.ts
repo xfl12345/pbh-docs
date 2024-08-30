a347cd066fb4fb94c9f49ac0c434a71a192a863b
Ghost_chu <ghostchu@111.com>
2024-08-31 04:01:28 +0800

    改进文档

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://pbh-btn.github.io/pbh-docs',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/pbh-docs/',
@@ -57,7 +57,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/PBH-BTN/pbh-docs/edit/',
+            'https://github.com/PBH-BTN/pbh-docs/edit/master',
         },
         theme: {
           customCss: './src/css/custom.css',
